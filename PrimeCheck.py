932a3e233fbb807c9c00d62f36c5aa4e48b3e7cb
onahte <76444607+onahte@users.noreply.github.com>
2020-12-31 22:15:07 -0500

    Update PrimeCheck.py

--- PrimeCheck.py
+++ PrimeCheck.py
@@ -1,13 +1,12 @@
 def prime_checker(number):
     prime_check = 'True'
-    for x in range(2, number + 1):
+    for x in range(2, number):
         if number % x == 0:
-            if x != number:
-                prime_check = 'False'
-    if prime_check == 'False':
-        print('not prime')
+            prime_check = 'False'
+    if prime_check:
+        print('This number is prime.')
     else:
-        print('prime')
+        print('This number is not prime.')
 
 n = int(input("Check this number: "))
 prime_checker(number=n)
