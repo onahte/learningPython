d5f7210f7f8c719d16a09209e0faef61709e7847
onahte <76444607+onahte@users.noreply.github.com>
2021-01-01 21:07:56 -0500

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -16,19 +16,20 @@ operations = {
   '*' : multiply,
   '/' : divide,
 }
-
-num1 = int(input('Enter a number: '))
-operations_str = ""
-for key in operations:
-  operations_str += key + " "
-print(operations_str)
-cont = 'True'
-while cont:
-  operation_input = input('Select an operand: ')
-  num2 = int(input('Another number: '))
-  answer = operations[operation_input](num1, num2)
-  print(f'{num1} {operation_input} {num2} = {answer}')
-  num1 = answer
-  calc = input("Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
-  if calc != 'y':
-    cont = False
+def calculator():
+    num1 = float(input('Enter a number: '))
+    operations_str = ""
+    for key in operations:
+      operations_str += key + " "
+    print(operations_str)
+    cont = 'True'
+    while cont:
+      operation_input = input('Select an operand: ')
+      num2 = float(input('Another number: '))
+      answer = operations[operation_input](num1, num2)
+      print(f'{num1} {operation_input} {num2} = {answer}')
+      num1 = answer
+      calc = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit: ")
+      if calc == 'n':
+        calculator()
+calculator()
